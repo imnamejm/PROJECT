7084c93d9a78e836f54fff0d8f1a38df77e3af8f
imnamejm(Lect) <143979787+imnamejm@users.noreply.github.com>
2023-11-12 22:54:28 +0900

    targetmove
    pattern1 구현

--- BasketBall_King/target.py
+++ BasketBall_King/target.py
@@ -1,13 +1,21 @@
 from pico2d import *
+import game_world
+import game_framework
 
 class Target:
-    def __init__(self, x = 600, y = 600, velocity = 1):
+    def __init__(self, x = 600, y = 600, pattern = 1, velocity = 1):
         self.image = load_image('target.png')
-        self.x, self.y, self.velocity = x, y, velocity
+        self.x, self.y, self.pattern, self.velocity = x, y, pattern, velocity
 
 
     def update(self):
-        pass
+        if self.pattern == 1:
+            if self.x > 750:
+                self.velocity = -1
+            elif self.x < 450:
+                self.velocity = 1
+            self.x += self.velocity * 50 * game_framework.frame_time
+
 
     def draw(self):
         self.image.draw(self.x, self.y)
