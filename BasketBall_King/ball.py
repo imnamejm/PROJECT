b91bfe38b3841b26a13656bd4a272ecd6f6facb6
imnamejm(Lect) <143979787+imnamejm@users.noreply.github.com>
2023-11-12 22:22:30 +0900

    mousemove
    마우스로 공을 이동시키기를 구현하려다 실패

--- BasketBall_King/ball.py
+++ BasketBall_King/ball.py
@@ -1,13 +1,21 @@
 from pico2d import *
+import game_framework
+import game_world
+
+def pick_ball(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_MOUSEBUTTONDOWN and e[1].type == SDL_BUTTON_LEFT
+
+def shoot_ball(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_MOUSEBUTTONUP and e[1].type == SDL_BUTTON_LEFT
 
 class Ball:
     image = None
 
-    def __init__(self, x = 600, y = 200, velocity = 1):
+    def __init__(self):
         if Ball.image == None:
             Ball.image = load_image('basicball.png')
-            self.x, self.y, self.velocity = x, y, velocity
-
+            self.state_machine = StateMachine(self)
+            self.x, self.y, self.velocity = 600, 100, 1
 
     def draw(self):
         self.image.draw(self.x, self.y)
@@ -16,6 +24,74 @@ class Ball:
     def update(self):
         pass
 
+    def handle_event(self, event):
+       pass
+
     def get_bb(self):
         return self.x - 50, self.y - 50, self.x + 50, self.y + 50
 
+    def put_mouse(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Idle:
+    @staticmethod
+    def select(ball, e):
+        pass
+
+    @staticmethod
+    def shoot(ball, e):
+        pass
+
+    @staticmethod
+    def stop(ball):
+        pass
+
+    @staticmethod
+    def draw(ball):
+        pass
+
+class Grab:
+    @staticmethod
+    def select(ball, e):
+        pass
+    @staticmethod
+    def shoot(ball, e):
+        pass
+
+    @staticmethod
+    def stop(ball):
+        pass
+
+    @staticmethod
+    def draw(ball):
+        pass
+
+class StateMachine:
+    def __init__(self, ball):
+        pass
+#        self.ball = ball
+#        self.cur_state = Idle
+#        self.transitions = {
+#            Idle: {pick_ball: Grab, shoot_ball: Grab},
+#            Grab: {pick_ball: Idle, shoot_ball: Idle}
+#        }
+    def update(self):
+        pass
+#        self.cur_state.stop(self.ball)
+
+
+    def handle_event(self, e):
+        pass
+#        for check_event, next_state in self.transitions[self.cur_state].items():
+#            if check_event(e):
+#                self.cur_state.select(self.ball, e)
+#                self.cur_state = next_state
+#                self.cur_state.shoot(self.ball, e)
+#                return True
+#        return False
+
+    def draw(self):
+        pass
+#        self.cur_state.draw(self.ball)
--- BasketBall_King/play_mode.py
+++ BasketBall_King/play_mode.py
@@ -6,13 +6,25 @@ from ball import Ball
 from target import Target
 from field import Field
 
+field_x, field_y = 1200, 800
+
 def handle_events():
     events = get_events()
+    global x, y
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, field_y - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            x, y = event.x, field_y - 1 - event.y
+            ball.put_mouse(ball, x, y)
+            hide_cursor()
+
 
 def init():
     global field
